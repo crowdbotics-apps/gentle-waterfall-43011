ee135d6c240f5b9ccd1e3872996eacfb7f7a3a91
Crowdbotics <team@crowdbotics.com>
2023-08-21 22:25:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-waterfall-43011",
+  url: "https://gentle-waterfall-43011.botics.co",
+  api: "https://gentle-waterfall-43011.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
