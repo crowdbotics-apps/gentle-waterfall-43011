aae510cde6417b45faf5cdcabe941f66247e4b2b
ShivaPandey02 <shiva.pandey@crowdbotics.com>
2023-09-02 20:01:28 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI24/index.js
+++ screens/ScreenAI24/index.js
@@ -1,24 +1,26 @@
 import { StyleSheet } from "react-native";
-import React, { useState } from 'react';
-import { SafeAreaView, View, Text, Button, FlatList, Image, ScrollView } from 'react-native';
+import React, { useState } from "react";
+import { SafeAreaView, View, Text, TouchableOpacity, FlatList, Image, ScrollView } from "react-native";
 
 const SessionScreen = () => {
   const [selectedSession, setSelectedSession] = useState(null);
   const sessions = [{
-    id: '1',
-    name: 'Session 1',
+    id: "1",
+    name: "Session 1",
     painLevel: 3,
     stepLevel: 2
   }, {
-    id: '2',
-    name: 'Session 2',
+    id: "2",
+    name: "Session 2",
     painLevel: 5,
     stepLevel: 4
   } // Add more sessions as needed
   ];
   return <SafeAreaView style={_styles.iJgxMQne}>
       <View style={_styles.TmLBGOEb}>
-        <Button title="Start new session" onPress={() => {}} />
+        <TouchableOpacity style={_styles.button} onPress={() => {}}>
+          <Text style={_styles.buttonText}>Start new session</Text>
+        </TouchableOpacity>
       </View>
       <View style={_styles.HrmPYOcl}>
         <Text style={_styles.FqrULQjp}>Previous Sessions</Text>
@@ -26,16 +28,22 @@ const SessionScreen = () => {
         item
       }) => <View style={_styles.dHXmPflL}>
               <Text style={_styles.aeXlVqiz}>{item.name}</Text>
-              <Button title="View Details" onPress={() => setSelectedSession(item)} />
+              <TouchableOpacity style={_styles.button} onPress={() => setSelectedSession(item)}>
+                <Text style={_styles.buttonText}>View Details</Text>
+              </TouchableOpacity>
             </View>} />
       </View>
       {selectedSession && <ScrollView style={_styles.GrNYOOHT}>
           <Text style={_styles.mdceUNvF}>Session Details</Text>
           <Image style={_styles.wYBTTWRu} source={{
-        uri: 'https://tinyurl.com/42evm3m3'
+        uri: "https://tinyurl.com/42evm3m3"
       }} />
-          <Text style={_styles.uuuwTLpl}>Pain Level: {selectedSession.painLevel}</Text>
-          <Text style={_styles.NdOUqjxt}>Step Level: {selectedSession.stepLevel}</Text>
+          <Text style={_styles.uuuwTLpl}>
+            Pain Level: {selectedSession.painLevel}
+          </Text>
+          <Text style={_styles.NdOUqjxt}>
+            Step Level: {selectedSession.stepLevel}
+          </Text>
         </ScrollView>}
     </SafeAreaView>;
 };
@@ -83,5 +91,16 @@ const _styles = StyleSheet.create({
   NdOUqjxt: {
     fontSize: 18,
     marginTop: 20
+  },
+  button: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 5,
+    marginTop: 10
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16
   }
 });
--- screens/ScreenAI26/index.js
+++ screens/ScreenAI26/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { SafeAreaView, View, Text, Button, StyleSheet, Image } from 'react-native';
+import React, { useState } from "react";
+import { SafeAreaView, View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
 
 const PainRatingScreen = () => {
   const [painRating, setPainRating] = useState(0);
@@ -18,37 +18,61 @@ const PainRatingScreen = () => {
       <View style={styles.ratingContainer}>
         {Array.from({
         length: 11
-      }, (_, i) => <Button key={i} title={i.toString()} onPress={() => handlePainRating(i)} color={painRating === i ? 'blue' : 'gray'} />)}
+      }, (_, i) => <TouchableOpacity key={i} style={painRating === i ? styles.buttonActive : styles.button} onPress={() => handlePainRating(i)}>
+              <Text style={styles.buttonText}>{i.toString()}</Text>
+            </TouchableOpacity>)}
       </View>
-      <Button title="Start Therapy" onPress={() => console.log('Start Therapy')} disabled={painRating !== 10} />
-      <Button title="Start Second Round of Rating" onPress={() => console.log('Start Second Round of Rating')} disabled={painRating !== 10} />
+      <TouchableOpacity style={styles.button} onPress={() => console.log("Start Therapy")} disabled={painRating !== 10}>
+        <Text style={styles.buttonText}>Start Therapy</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => console.log("Start Second Round of Rating")} disabled={painRating !== 10}>
+        <Text style={styles.buttonText}>Start Second Round of Rating</Text>
+      </TouchableOpacity>
       <Image style={styles.ekgImage} source={{
-      uri: 'https://tinyurl.com/42evm3m3'
+      uri: "https://tinyurl.com/42evm3m3"
     }} />
-      <Button title={isEKGRunning ? 'Stop EKG' : 'Start EKG'} onPress={handleEKG} />
+      <TouchableOpacity style={styles.button} onPress={handleEKG}>
+        <Text style={styles.buttonText}>{isEKGRunning ? "Stop EKG" : "Start EKG"}</Text>
+      </TouchableOpacity>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center"
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20
   },
   ratingContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginBottom: 20
   },
+  button: {
+    backgroundColor: "gray",
+    padding: 10,
+    margin: 5,
+    borderRadius: 5
+  },
+  buttonActive: {
+    backgroundColor: "blue",
+    padding: 10,
+    margin: 5,
+    borderRadius: 5
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center"
+  },
   ekgImage: {
-    width: '100%',
+    width: "100%",
     height: 200,
-    resizeMode: 'contain',
+    resizeMode: "contain",
     marginBottom: 20
   }
 });
